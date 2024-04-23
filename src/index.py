f9ff7d333c1830d588f00cd838fab7375c316b94
kyzooghost <jeffersonzjtang@gmail.com>
2024-04-23 16:47:02 +1000

    add more structure

--- src/index.py
+++ src/index.py
@@ -1,12 +1,16 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, ConversationHandler
 import os
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
-TG_TOKEN = os.environ.get("TG_TOKEN")
-app = ApplicationBuilder().token(TG_TOKEN).build()
-app.add_handler(CommandHandler("hello", hello))
-print("Bot started")
-app.run_polling()
+def main():
+    TG_TOKEN = os.environ.get("TG_TOKEN")
+    app = ApplicationBuilder().token(TG_TOKEN).build()
+    app.add_handler(CommandHandler("hello", hello))
+    print("Bot started")
+    app.run_polling()
+
+if __name__ == '__main__':
+    main()
