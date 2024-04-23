fba1f66a95d09f6af95fcf0fbb3f5aac37895b2e
kyzooghost <jeffersonzjtang@gmail.com>
2024-04-23 23:53:46 +1000

    write some of write.py

--- src/handlers/write.py
+++ src/handlers/write.py
@@ -1,5 +1,65 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
+import logging
+logger = logging.getLogger(__name__)
 
-async def write(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+# https://docs.python-telegram-bot.org/en/v21.1.1/examples.conversationbot.html
+
+MESSAGE_KEY, MESSAGE_VALUE = range(2)
+
+async def write(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Starts the conversation - ask to input a message key."""
+
+    await update.message.reply_text(
+        "Hi! I will save your messages. Please enter a message tag:"
+    )
+
+    return MESSAGE_KEY
+
+async def message_key(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Starts the conversation - ask to input a message key."""
+
+    await update.message.reply_text(
+        "Please enter a message:"
+    )
+
+    return MESSAGE_VALUE
+
+async def message_value(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the info about the user and ends the conversation."""
+    user = update.message.from_user
+    logger.info("Bio of %s: %s", user.first_name, update.message.text)
+    await update.message.reply_text("Thank you! Your message has been saved")
+    return ConversationHandler.END
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Cancels and ends the conversation."""
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    await update.message.reply_text(
+        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ConversationHandler.END
+
+handler = ConversationHandler(
+    entry_points=[CommandHandler("write", write)],
+    states={
+        MESSAGE_KEY: [MessageHandler(filters.TEXT & ~filters.COMMAND, message_key)],
+        MESSAGE_VALUE: [MessageHandler(filters.TEXT & ~filters.COMMAND, message_value)],
+    },
+    fallbacks=[CommandHandler("cancel", cancel)],
+)
+
+# async def write(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+# async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
--- src/index.py
+++ src/index.py
@@ -1,7 +1,8 @@
-from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters, InlineQueryHandler
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 import os
 import logging
+from handlers import fallback_command
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -11,45 +12,21 @@ logging.basicConfig(
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
-# @param update - TG update
-# @param context - Current TG context, including Bot, Application, library etc
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # Bot class available in `context.bot`
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
-async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.inline_query.query
-    if not query:
-        return
-    results = []
-    results.append(
-        InlineQueryResultArticle(
-            id=query.upper(),
-            title='Caps',
-            input_message_content=InputTextMessageContent(query.upper())
-        )
-    )
-    await context.bot.answer_inline_query(update.inline_query.id, results)
-
-async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
-
 def main():
     TG_TOKEN = os.environ.get("TG_TOKEN")
 
-    # Application class 
-    # - Create Updater class
-    # - Create `update_queue` - where Updater class retrieve TG updates
-    # - Register handlers with Application class
-    # - Each handler filter updates fetched, and deliver to callback function
     app = ApplicationBuilder().token(TG_TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), echo))
     app.add_handler(CommandHandler("hello", hello))
-    app.add_handler(InlineQueryHandler(inline_caps))
-    app.add_handler(MessageHandler(filters.COMMAND, unknown))
+
+    # Fallback
+    app.add_handler(MessageHandler(filters.COMMAND, fallback_command.handler))
     app.run_polling()
 
 if __name__ == '__main__':
